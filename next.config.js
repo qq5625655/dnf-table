11fe48c294c2fa6f35747df0a82e96fec14da89a
zhangxusheng <1024694790@qq.com>
2023-05-20 17:45:55 +0800

    改为jsx

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    rewrites: async () => [
-        {
-          source: '/',
-          destination: '/app/page',
-        },
-        {
-            source: '/main',
-            destination: '/app/loggingData/page'
-        },
-      ],
+    
 }
 
 module.exports = nextConfig
